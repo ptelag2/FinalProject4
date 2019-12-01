4a0df1e0b3fb69ced00aae40c572c35ed554ffdb
ptelag2 <ptelag2@illinois.edu>
2019-12-01 13:04:26 -0600

    First COmmit

--- app/src/main/java/com/example/finalproject3/MainActivity.java
+++ app/src/main/java/com/example/finalproject3/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.finalproject3;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startBtn = findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("HEllo", "HELLO");
+            }
+        });
     }
 }
