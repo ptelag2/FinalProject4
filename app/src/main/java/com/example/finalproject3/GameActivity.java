c6c2101d815c20148c154c36cc39e81f82bb02bf
ptelag2 <ptelag2@illinois.edu>
2019-12-01 13:18:21 -0600

    Game Actvity and Screen added

---
+++ app/src/main/java/com/example/finalproject3/GameActivity.java
@@ -0,0 +1,19 @@
+package com.example.finalproject3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_screen);
+
+
+
+
+    }
+
+}
