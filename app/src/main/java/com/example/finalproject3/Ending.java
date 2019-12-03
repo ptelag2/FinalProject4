868132dc1404a3b9fafc9986f880f31bd92116a3
drinkingtea2223 <dhan78557@gmail.com>
2019-12-03 09:44:51 -0600

    ending screen

--- app/src/main/java/com/example/finalproject3/Ending.java
+++ app/src/main/java/com/example/finalproject3/Ending.java
@@ -1,8 +1,35 @@
 package com.example.finalproject3;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Ending  extends AppCompatActivity {
 
+    Button playAgain;
+    TextView scoring;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ending_screen);
 
+        playAgain = findViewById(R.id.againbtn);
+        scoring = findViewById(R.id.scoring);
+        scoring.setText("")
+
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchGame();
+            }
+        });
+    }
+    public void launchGame() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
 }
